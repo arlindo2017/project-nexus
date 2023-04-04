c4a6f8f51a7e6a0cc728cd7038e9b4977750aaf7
Mirek <nyziu@DESKTOP-N9P8O81>
2023-04-03 20:51:21 -0400

    updated model index.js file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,43 @@
-const { User, Post, Answer, Tag }= require('./models');
+const { User, Post, Answer, Tag } = require('./models');
 
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+User.hasMany(Answer, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Answer.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
+});
+
+Post.hasMany(Answer, {
+  foreignKey: 'post_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+Answer.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+Post.hasOne(Tag, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Tag.belongsTo(Post, {
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
 });
 
 module.exports = { User, Project };
