7fe4a90f19bf05fe38fb32bd14bf3b4f30c29dfb
Arlindo Pinto <perensen@gmail.com>
2023-04-06 20:15:08 -0400

    added ability to flag posts

---
+++ public/js/flagPost.js
@@ -0,0 +1,17 @@
+const flagPostBtnEl = document.querySelectorAll(".flag-post-btn");
+
+flagPostBtnEl.forEach((btn) => {
+  btn.addEventListener("click", async () => {
+    const postId = btn.dataset.postId;
+    try {
+      console.log(postId);
+      await fetch(`api/posts/flag/${postId}`, {
+        method: "POST",
+      });
+      // Reload page after deleting answer.
+      document.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  });
+});
