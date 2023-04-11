39cf76bfb7b0631bc50bff2295802956b3d8de00
Arlindo Pinto <perensen@gmail.com>
2023-04-10 22:22:48 -0400

    fixed profile links

--- controllers/api/tagRoutes.js
+++ controllers/api/tagRoutes.js
@@ -25,7 +25,7 @@ router.get("/:id", async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ["name"],
+          attributes: ["name", "user_img"],
         },
       ],
       attributes: [
